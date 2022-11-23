5f7a034403a2fb69ec92c62b4a8435b10c8e6794
podi <podipeti17@gmail.com>
2022-11-23 15:09:40 +0100

    week10

---
+++ FD64YT_week10/JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FD64YT_week10.JokeModels;
+
+public partial class Joke
+{
+    public int JokeSk { get; set; }
+
+    public string? JokeText { get; set; }
+
+    public int UpVotes { get; set; }
+
+    public int DownVotes { get; set; }
+}
