3a2ff8a3e5a4dc56db9e543213da414431fddfb7
podi <podipeti17@gmail.com>
2022-12-08 03:46:02 +0100

    week12

--- FD64YT_week10/wwwroot/jsgyak.js
+++ FD64YT_week10/wwwroot/jsgyak.js
@@ -5,32 +5,31 @@
 function factorialize(n) {                    // recursive
     if (n < 0)
         return 0;
-
     else if (n == 0 || n == 1)
         return 1;
-
     else {
-        return (n * factorialize(n - 1));
+        return (n * factorialize(n - 1))
     }
 }
 
 
 function pascal() {
-    let n = document.getElementById("input").value;
-
     for (var row = 0; row < 10; row++) {
-        let ogDiv = document.getElementById("pascal")
-        let newRow = document.createElement("div")
-        newRow.classList.add("sor")
+        let ogDiv = document.getElementById("pascal");
+        let newRow = document.createElement("div");
+        newRow.classList.add("sor");
         ogDiv.appendChild(newRow);
         for (var col = 0; col <= row; col++) {
-            let newElement = document.createElement("div")
-            newElement.classList.add("elem")
+            let newElement = document.createElement("div");
+            newElement.classList.add("elem");
             newRow.appendChild(newElement);
-            newElement.innerHTML = factorialize(col)
+            var num = factorialize(row) / (factorialize(col) * factorialize(row - col))
+            newElement.innerHTML = num;
+            var fade = num / 100;
+            newElement.style.backgroundColor = "rgba(18, 176, 176, " + fade + ")";
         }
     }
-
-    document.getElementById("input").value = 0;
 }
+
+
 
